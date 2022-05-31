bbba2b59ca0f894c0b42d6e11005ca9da5059433
Joep Heijnen <joep.heijnen@topicus.nl>
2022-05-31 09:33:04 +0200

    Added tests

---
+++ test/main_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('1 + 1 = 2', () {
+    expect(1 + 1, 2);
+  });
+
+  test('2 + 2 = 4', () {
+    expect(2 + 2, 4);
+  });
+
+  test('3 + 3 = 6', () {
+    expect(3 + 3, 6);
+  });
+}
