94ee406911a1ce77bbcc89e845bca6f258e2bd2c
Joep Heijnen <joep.heijnen@topicus.nl>
2022-06-02 11:34:00 +0200

    Update

--- ios/RunnerTests/RunnerTests.m
+++ ios/RunnerTests/RunnerTests.m
@@ -1,11 +1,11 @@
-////
-////  RunnerTests.m
-////  RunnerTests
-////
-////  Created by Joep Heijnen on 01/06/2022.
-////
 //
-//@import XCTest;
-//@import integration_test;
+//  RunnerTests.m
+//  RunnerTests
 //
-//INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
+//  Created by Joep Heijnen on 01/06/2022.
+//
+
+@import XCTest;
+@import integration_test;
+
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
--- test-build-ios.sh
+++ test-build-ios.sh
@@ -1,1 +1,1 @@
-xcodebuild test-without-building -xctestrun "build/ios_integ/Build/Products/Runner_iphoneos15.4-arm64.xctestrun" -destination id=01CA9180-08B6-41EF-B6C9-3FFE74456293
+xcodebuild test-without-building -xctestrun "build/ios_integ/Build/Products/Runner_iphoneos15.4-arm64.xctestrun" -destination id=A83DDD36-2EF5-41B8-A314-88A1201D9ACB
