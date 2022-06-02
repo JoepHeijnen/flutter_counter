ddbf236f51f923de0bbdca2d0ea8f8b7507ea76a
Joep Heijnen <joep.heijnen@topicus.nl>
2022-06-02 16:22:11 +0200

    Update

--- test-build-ios.sh
+++ test-build-ios.sh
@@ -1,1 +1,3 @@
 xcodebuild test-without-building -xctestrun "build/ios_integ/Build/Products/Runner_iphoneos15.4-arm64.xctestrun" -destination id=A83DDD36-2EF5-41B8-A314-88A1201D9ACB
+
+gcloud firebase test ios run --test "build/ios_integ/Build/Products/ios_tests.zip" --xctestrun-file="build/ios_integ/Build/Products/Runner_iphoneos15.4-arm64.xctestrun"
